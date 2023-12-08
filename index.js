4e78cf315db78b386dd1ead82ed4c4a39d5334d4
okshi <johnstarhafiz@gmail.com>
2023-12-08 20:42:35 +0600

    Server side connect with MongoDB database successfully

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 //MiddleWare
 app.use(cors());
@@ -11,6 +12,32 @@ app.use(cors());
 //MiddleWare
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_SECRET}:${process.env.PASSWORD_SECRET}@cluster0.luy9u.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "OHHHH!!! Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hungry Place Server Runing !");
